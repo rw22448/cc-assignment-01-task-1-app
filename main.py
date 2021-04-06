36da791a24126e7252f65d4b346e889e966f593e
Richard Wang <rw22448@gmail.com>
2021-04-06 15:31:58 +1000

    Add message submit and display on forum

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def register():
         username = request.form['username']
         password = request.form['password']
         image = request.files['image']
-        filename = secure_filename(image.filename)
 
         for user in users:
             if id == user['id']:
@@ -61,9 +60,8 @@ def register():
                 error = 'The username already exists'
 
         if error is None:
-            # TODO: update filename to use id as unique identifier
             upload_image(
-                image, 'cc-assignment-01-task-1-app.appspot.com', filename)
+                image, 'cc-assignment-01-task-1-app.appspot.com', id)
 
             store_user(id, username, password)
             return redirect(url_for('login'))
@@ -71,14 +69,27 @@ def register():
     return render_template('register.html', error=error)
 
 
-@app.route('/forum')
+@app.route('/forum', methods=('GET', 'POST'))
 def forum():
     if not localStorage.getItem('user_id'):
         return redirect(url_for('login'))
 
     user_id = localStorage.getItem('user_id')
+    posts = fetch_posts(10)
 
-    return render_template('forum.html', user_id=user_id)
+    if request.method == 'POST':
+        subject = request.form['subject']
+        message = request.form['message']
+        image = request.files['image']
+        filename = secure_filename(image.filename)
+        current_datetime = datetime.datetime.now()
+
+        store_post(subject, message, subject +
+                   filename, current_datetime, user_id)
+        upload_image(
+            image, 'cc-assignment-01-task-1-app-message-images', subject + filename)
+
+    return render_template('forum.html', user_id=user_id, posts=posts)
 
 
 @app.route('/user-page')
@@ -94,11 +105,20 @@ def logout():
     return redirect(url_for('login'))
 
 
-def fetch_users():
+def fetch_users(limit=None):
     query = datastore_client.query(kind='user')
     query.order = ['id']
 
-    output = query.fetch()
+    output = query.fetch(limit=limit)
+
+    return list(output)
+
+
+def fetch_posts(limit=None):
+    query = datastore_client.query(kind='post')
+    query.order = ['-datetime']
+
+    output = query.fetch(limit=limit)
 
     return list(output)
 
@@ -109,6 +129,19 @@ def store_user(id, username, password):
         'id': id,
         'user_name': username,
         'password': password
+    })
+
+    datastore_client.put(entity)
+
+
+def store_post(subject, message, image_id, current_datetime, user_id):
+    entity = datastore.Entity(key=datastore_client.key('post'))
+    entity.update({
+        'subject': subject,
+        'message': message,
+        'image_id': image_id,
+        'datetime': current_datetime,
+        'user_id': user_id
     })
 
     datastore_client.put(entity)
